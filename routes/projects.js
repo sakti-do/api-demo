a9597d2be26c7962cfdc8d300f7a1fc0af747983
autoB0T <debnath.sakti@gmail.com>
2020-07-26 19:20:32 +0530

    done

--- routes/projects.js
+++ routes/projects.js
@@ -3,11 +3,12 @@ const e = require('express');
 var projects = data.getProjects();
 var router = require('express').Router();
 router.get('/',function (req, res, next) {
-    res.status(200).json(projects); 
+    return res.status(200).json(projects); 
 });
 router.get('/:id',function (req, res, next) {
+    var activeResult = [];
     if(req.params.id==='active'){
-        var activeResult = [];
+        
         projects.forEach(function(v){
             if(v.isActive){
                 activeResult.push(v);
@@ -15,13 +16,14 @@ router.get('/:id',function (req, res, next) {
         });
         return res.status(200).send(activeResult); 
     } else {
-        projects.forEach(function(v){
-            if(v.id==req.params.id){
-                return res.status(200).json(v);
-            }
+        activeResult = projects.filter(function(v){
+            return v.id==req.params.id;
         });
-    }    
-   return res.status(404).json([]); 
+        
+        let status = activeResult.length ? 200 :404;
+        activeResult = activeResult.length?activeResult[0]:{message : 'No Project Found'};
+        return res.status(status).json(activeResult);
+    }
 });
 
 module.exports = router;
