638fc843983c111b4f6b5a1fd7e088d56922ec3e
autoB0T <debnath.sakti@gmail.com>
2020-07-26 13:28:26 +0530

    added 3 routers

--- routes/projects.js
+++ routes/projects.js
@@ -1,6 +1,25 @@
 var data = require('../data-store');
 var projects = data.getProjects();
 var router = require('express').Router();
-
+router.get('/',function (req, res, next) {
+    res.send(projects); 
+});
+router.get('/:id',function (req, res, next) {
+    projects.forEach(function(v){
+        if(v.id==req.params.id){
+            return res.send(v);
+        }
+    });
+   return res.send([]); 
+});
+router.get('/active',function (req, res, next) {
+    var activeResult = [];
+    projects.forEach(function(v){
+        if(v.isActive){
+            activeResult.push(v);
+        }
+    });
+   return res.send(activeResult); 
+});
 
 module.exports = router;
